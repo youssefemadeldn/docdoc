ed7af01eb04c798e61ab46a22e794e2394c8c3e3
youssef emad eldin <youssefemadeldn>
2024-11-05 15:08:59 +0300

    - edited login data layer: remote data source & model - added login mapper

--- lib/features/login/data/models/login_response_model.dart
+++ lib/features/login/data/models/login_response_model.dart
@@ -1,10 +1,8 @@
 import 'package:json_annotation/json_annotation.dart';
 part 'login_response_model.g.dart';
 
-@JsonSerializable()
 class LoginResponseModel {
   String? message;
-  @JsonKey(name: 'data')
   UserDataModel? userDataModel;
   bool? status;
   int? code;
@@ -12,8 +10,22 @@ class LoginResponseModel {
   LoginResponseModel(
       {this.message, this.userDataModel, this.status, this.code});
 
-  factory LoginResponseModel.fromJson(Map<String, dynamic> json) =>
-      _$LoginResponseModelFromJson(json);
+  factory LoginResponseModel.fromJson(dynamic json) {
+    // Handle `data` field based on its type
+    UserDataModel? userData;
+    if (json['data'] is Map<String, dynamic>) {
+      userData = UserDataModel.fromJson(json['data']);
+    } else if (json['data'] is List && json['data'].isEmpty) {
+      userData = null;
+    }
+
+    return LoginResponseModel(
+      message: json['message'] as String?,
+      userDataModel: userData,
+      status: json['status'] as bool?,
+      code: json['code'] as int?,
+    );
+  }
 }
 
 @JsonSerializable()
@@ -24,6 +36,5 @@ class UserDataModel {
 
   UserDataModel({this.token, this.userName});
 
-  factory UserDataModel.fromJson(Map<String, dynamic> json) =>
-      _$UserDataModelFromJson(json);
+  factory UserDataModel.fromJson(dynamic json) => _$UserDataModelFromJson(json);
 }
