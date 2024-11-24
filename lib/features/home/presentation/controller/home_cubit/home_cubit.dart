e65d10bf5f06702b890de0714f959de87bbca488
youssef emad eldin <youssefemadeldn>
2024-11-24 15:55:39 +0200

    Add HomeCubit and HomeState for managing home data flow

---
+++ lib/features/home/presentation/controller/home_cubit/home_cubit.dart
@@ -0,0 +1,30 @@
+import 'package:bloc/bloc.dart';
+import 'package:docdoc/core/error/failure.dart';
+import 'package:docdoc/core/network/network_helper.dart';
+import 'package:docdoc/features/home/domain/entities/home_response_entity.dart';
+import 'package:docdoc/features/home/domain/use_cases/home_use_case.dart';
+import 'package:meta/meta.dart';
+
+part 'home_state.dart';
+
+class HomeCubit extends Cubit<HomeState> {
+  HomeUseCase homeUseCase;
+
+  HomeCubit({required this.homeUseCase}) : super(HomeInitialState());
+
+  void getHomeData() async {
+    emit(SpeciallyLoadingState());
+
+    var isConnected = await NetworkHelper.checkInternet();
+    if (!isConnected) {
+      emit(HomeDiscountedInternetState());
+    } else {
+      var either = await homeUseCase.getSpeciallyDoctors();
+
+      either.fold(
+        (failure) => emit(HomeFailureState(failure: failure)),
+        (response) => emit(HomeSuccessState(homeResponseEntity: response)),
+      );
+    }
+  }
+}
