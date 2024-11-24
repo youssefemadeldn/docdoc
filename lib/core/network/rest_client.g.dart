6566b9d9ec9de6e94cb0661e6c2c96109c236789
youssef emad eldin <youssefemadeldn>
2024-11-24 15:50:37 +0200

    Add home endpoint to RestClient and update API constants

--- lib/core/network/rest_client.g.dart
+++ lib/core/network/rest_client.g.dart
@@ -93,6 +93,39 @@ class _RestClient implements RestClient {
     return _value;
   }
 
+  @override
+  Future<HomeResponseModel> getSpeciallyDoctors() async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<HomeResponseModel>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/specialization/index',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late HomeResponseModel _value;
+    try {
+      _value = HomeResponseModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
