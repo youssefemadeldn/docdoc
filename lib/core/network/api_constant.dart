02bdb4b47d6d1c9a554619995d831f87b6cf9ca3
youssef emad eldin <youssefemadeldn>
2025-01-09 11:24:01 +0200

    implement ifUserLoggedIn

--- lib/core/network/api_constant.dart
+++ lib/core/network/api_constant.dart
@@ -1,5 +1,9 @@
 class ApiConstant {
+  // Bases
   static const String baseUrl = 'https://vcare.integration25.com/api/';
+  static const String userToken = '';
+
+  // EntPoints
   static const String loginEP = '/auth/login';
   static const String signUpEP = '/auth/register';
   static const String homeEP = '/specialization/index';
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:docdoc/core/cache/shared_pref.dart';
 import 'package:docdoc/core/di/di.dart';
+import 'package:docdoc/core/helper/constants.dart';
 import 'package:docdoc/core/routes/app_router.dart';
 import 'package:docdoc/doc_doc_app.dart';
 import 'package:docdoc/my_bloc_observer.dart';
@@ -8,14 +10,29 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //  Shared pref init
+  await SharedPrefUtils.init();
+
   // to fix text being hidden in release mode
   await ScreenUtil.ensureScreenSize();
+
+  // Bloc observer
   Bloc.observer = MyBlocObserver();
 
   configureDependencies();
+  checkIfUserLoggedIn();
   runApp(
     DocDocApp(
       appRouter: AppRouter(),
     ),
   );
+}
+
+void checkIfUserLoggedIn() {
+  var token = SharedPrefUtils.getData('token');
+  if (token != null && token.isNotEmpty) {
+    isLoggedIn = true;
+  } else {
+    isLoggedIn = false;
+  }
 }
